1e99262236b1acd86ef16e0340ce2b445ed1d8ae
jsaj <j.jr.avelino@gmail.com>
2023-09-27 12:10:03 -0300

    catalog commit 0.07

--- conexao_mysql.py
+++ conexao_mysql.py
@@ -1,45 +1,36 @@
 
-import mysql.connector as mysql
+# import mysql.connector as mysql
 import pandas as pd
-
+import psycopg2
 # Configurações de conexão
 
 
-class conexao_mysql():
-
-    # def conectar_banco(self):
-    #     # Configurações de conexão
-    #     host = 'aws.connect.psdb.cloud'
-    #     user = 'glijhrkku43houa3oewn'
-    #     password = 'pscale_pw_k5HpOePAQkXH0XNiUNBLnDqaVTlS2jvfdKd62S61zMc'
-    #     database = 'streamlit-study-db'
-    #
-    #     # Tente estabelecer a conexão
-    #     conexao = mysql.connect(
-    #         host=host,
-    #         user=user,
-    #         password=password,
-    #         database=database
-    #     )
-    #
-    #     return conexao
+class conexao_banco():
     def conectar_banco(self):
         # Defina as informações de conexão
-        host = "sql10.freesqldatabase.com"
-        database = "sql10649389"
-        user = "sql10649389"
-        password = "J6UTmuFcCR"
-        port = 3306
+        # host = "sql10.freesqldatabase.com"
+        # database = "sql10649389"
+        # user = "sql10649389"
+        # password = "J6UTmuFcCR"
+        # port = 3306
+        #
+        #     # Conecte-se ao banco de dados
+        # connection = mysql.connect(
+        #     host=host,
+        #     database=database,
+        #     user=user,
+        #     password=password,
+        #     port=port)
 
-            # Conecte-se ao banco de dados
-        connection = mysql.connect(
-            host=host,
-            database=database,
-            user=user,
-            password=password,
-            port=port)
-            
+
+        # Defina a string de conexão com o banco de dados PostgreSQL
+        CONNSTR = f'postgresql://j.jr.avelino:FNOwP1MzaZ8h@ep-black-credit-89791997.us-west-2.aws.neon.tech/neondb'
+
+        # Conecte-se ao banco de dados PostgreSQL usando psycopg2
+        connection = psycopg2.connect(CONNSTR)
+
         return connection
+
     def insert_data(self, connection, tabela, colunas, valores):
 
         cursor = connection.cursor()
